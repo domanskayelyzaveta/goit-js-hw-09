6a6b769072e111a6181430ab0a64871ab06cca18
domanskayelyzaveta <130005425+domanskayelyzaveta@users.noreply.github.com>
2023-09-07 17:37:38 +0000

    Deploying to gh-pages from @ domanskayelyzaveta/goit-js-hw-09@b14bb6ff5ca2b55a44f7f343f7c61dfec7754152 🚀

---
+++ 01-color-switcher.3701768e.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e=123;t.startBtn.addEventListener("click",(()=>{e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.3701768e.js.map
